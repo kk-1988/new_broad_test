b4a8b999da885afb0ea3ece72a99cd6a9262e477
xiaobo.kong <xiaobo.kong@aliyun.com>
2020-02-02 11:00:48 +0800

    基于imx6ull的qemu的led驱动框架，自测通过

---
+++ 02_led_drv/01_led_drv_template/board_demo.c
@@ -0,0 +1,30 @@
+#include <linux/gfp.h>
+#include "led_opr.h"
+
+/*
+* 初始化LED，which - 哪个led
+*/
+static int board_demo_led_init(int which)
+{
+	printk("%s %s line %d, led %d\n",__FILE__,__FUNCTION__,__LINE__, which);
+	return 0;
+}
+
+/*
+* 指定哪盏灯进行控制,status 1 打开 0 关闭
+*/
+static int borad_demo_led_ctl(int which, char status)
+{
+	printk("%s %s line %d, led %d  %d %s\n",__FILE__,__FUNCTION__,__LINE__, which, status, status ? "on":"off");
+	return 0;
+}
+
+static struct led_operations board_demo_led_opr = {
+	.init = board_demo_led_init,
+	.ctl = borad_demo_led_ctl,
+};
+
+struct led_operations *board_demo_opr_get(void)
+{
+	return &board_demo_led_opr;
+}
---
+++ 02_led_drv/01_led_drv_template/led_opr.h
@@ -0,0 +1,14 @@
+#ifndef __LED_OPR_H__
+#define __LED_OPR_H__
+
+struct led_operations{
+	int (*init) (int which);	/* 初始化LED，which-哪个LED */
+	int	(*ctl)(int which, char status);	/* 控制LED，which-哪个LED，status:1 亮 0 灭*/
+};
+
+/*
+* 获得单板相关的结构体指针
+*/
+struct led_operations *board_demo_opr_get(void);
+
+#endif
